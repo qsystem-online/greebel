0c23de7b21bd49d72cb0d96eff3f0e5b75e9dd49
Enny <enny.nuraini06@gmail.com>
2019-05-10 09:49:13 +0700

    enny06 2019/05/10 09.48 Update form msrelations (delete source code yg tidak digunakan).

--- application/views/pages/pr/msrelations/form.php
+++ application/views/pages/pr/msrelations/form.php
@@ -425,13 +425,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 					}
 				});
 
-				$("#BirthDate").datepicker('update', dateFormat(resp.ms_relations.BirthDate));
-
 				var relationType = resp.ms_relations.RelationType.split(",");
 				console.log(relationType);
 				$("#RelationType").val(relationType).trigger('change');
 
-				//$('#RelationType').append(newOption).trigger('change');
+				$("#BirthDate").datepicker('update', dateFormat(resp.ms_relations.BirthDate));
 
 				// menampilkan data di select2, menu edit/update
 				var newOption = new Option(resp.ms_relations.CountryName, resp.ms_relations.CountryId, true, true);
