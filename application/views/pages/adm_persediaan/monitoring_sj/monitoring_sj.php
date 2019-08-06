45a261f10792d20f137cd491bd72c41232bfb8af
Enny <enny.nuraini06@gmail.com>
2019-08-06 16:30:50 +0700

    enny06 2019/08/06 16.30 Update monitoring_sj

--- application/views/pages/adm_persediaan/monitoring_sj/monitoring_sj.php
+++ application/views/pages/adm_persediaan/monitoring_sj/monitoring_sj.php
@@ -28,7 +28,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                     </ul>
 
                     <div class="tab-content">
-                        <div class="tab-pane active" id="tab_1" style="overflow-x:auto;">
+                        <div class="tab-pane active" id="tab_1">
 						<div align="right">					
 							<span>Search on:</span>
 							<span>
@@ -38,7 +38,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 								</select>
 							</span>
 						</div>
-                            <table id="tblMonitoring" style="width:100%"></table>
+                            <table id="tblMonitoring" class="display nowrap" style="width:100%"></table>
                         </div> <!-- /.tab-pane -->
 
                         <div class="tab-pane" id="tab_2">
@@ -53,7 +53,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                                     <input type="text" class="form-control datepicker filterData" id="end-date">
                                 </div>
                             </div>
-                            <table id="tblHistMonitoring" style="width:100%"></table>
+                            <table id="tblHistory" class="display nowrap" style="width:100%"></table>
                         </div><!-- /.tab-pane -->
                                             
                     </div> <!-- /.tab-content -->                    
@@ -64,27 +64,84 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     </div>
 </section>
 
+<div id="resiModal" class="modal fade" role="dialog">
+	<div class="modal-dialog" style="display:table;width:35%;min-width:350px;max-width:100%">	
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal">&times;</button>
+				<h4 class="modal-title"><?=lang("Update Resi")?></h4>
+			</div>
+
+			<div class="modal-body">
+				<form  class="form-horizontal">
+					<div class="form-group hide">
+						<label for="fin_sj_id" class="col-md-4 control-label"><?=lang("S/J ID")?></label>
+						<div class="col-md-8">
+							<input type="text" class="form-control text-right" id="fin_sj_id" name="fin_sj_id">
+							<div id="fin_sj_id_err" class="text-danger"></div>
+						</div>
+					</div>
+
+					<div class="form-group">
+						<label for="fst_sj_return_resi_no" class="col-md-4 control-label"><?=lang("S/J Resi No.")?></label>
+						<div class="col-md-8">
+							<input type="text" class="form-control text-right" id="fst_sj_return_resi_no" name="fst_sj_return_resi_no">
+							<div id="fst_sj_return_resi_no_err" class="text-danger"></div>
+						</div>
+					</div>
+
+					<div class="form-group">
+					<label for="fdt_sj_return_datetime" class="col-md-4 control-label"><?=lang("S/J Return Date")?></label>
+						<div class="col-md-8">
+							<div class="input-group date">
+								<div class="input-group-addon">
+									<i class="fa fa-calendar"></i>
+								</div>
+								<input type="text" class="form-control pull-right datepicker" id="fdt_sj_return_datetime" name="fdt_sj_return_datetime"/>								
+							</div>
+							<div id="fdt_sj_return_datetime_err" class="text-danger"></div>
+							<!-- /.input group -->
+						</div>
+					</div>
+				</form>
+			</div>
+
+			<div class="modal-footer">
+				<button id="btn-resi" type="button" class="btn btn-primary" ><?=lang("Update")?></button>
+				<button type="button" class="btn btn-default" data-dismiss="modal"><?=lang("Close")?></button>
+			</div>
+		</div>
+	</div>
+</div>
+
 <script type="text/javascript">
 	$(function(){
 		$("#tblMonitoring").DataTable({
+
+			"scrollX": true,
 			ajax: {
 				url:"<?=site_url()?>adm_persediaan/monitoring_sj/fetch_monitoring_list",
 			},
 			columns:[
                 {"title" : "S/J ID","width": "10%",sortable:true,data:"fin_sj_id",visible:true},
-				{"title" : "S/J No","width": "15%",sortable:true,data:"fst_sj_no",visible:true},
-                {"title" : "S/J Date","width": "15%",sortable:true,data:"fdt_sj_date",visible:true},
-				{"title" : "S/O No","width": "15%",sortable:true,data:"fst_salesorder_no",visible:true},
-				{"title" : "S/O Date","width": "15%",sortable:true,data:"fdt_salesorder_date",visible:true},
-                {"title" : "Gudang","width": "15%",sortable:true,data:"fst_warehouse_name",visible:true},
-				{"title" : "Cust","width": "20%",sortable:true,data:"fst_relation_name",visible:true},
-                {"title" : "Hold","width": "5%",sortable:true,data:"fbl_is_hold",visible:true},
-                {"title" : "Return Date","width": "15%",sortable:true,data:"fdt_sj_return_datetime",visible:true},
-                {"title" : "S/J Resi No","width": "20%",sortable:true,data:"fst_sj_return_resi_no",visible:true},
-				{"title" : "S/J Return Memo","width": "15%",sortable:true,data:"fst_sj_return_memo",visible:true},
-                {"title" : "S/J Return By ID","width": "15%",sortable:true,data:"fin_sj_return_by_id",visible:true},
-				{"title" : "Unhold Date","width": "0%",sortable:true,data:"fdt_unhold_datetime",visible:false},
-				{"title" : "Unhold","width": "15%",sortable:false,className:'dt-body-center text-center',
+				{"title" : "S/J No","width": "20%",sortable:true,data:"fst_sj_no",visible:true},
+                {"title" : "S/J Date","width": "20%",sortable:true,data:"fdt_sj_date",visible:true},
+				{"title" : "S/O No","width": "20%",sortable:true,data:"fst_salesorder_no",visible:true},
+				{"title" : "S/O Date","width": "20%",sortable:true,data:"fdt_salesorder_date",visible:true},
+                {"title" : "Gudang","width": "20%",sortable:true,data:"fst_warehouse_name",visible:true},
+				{"title" : "Customer","width": "25%",sortable:true,data:"fst_relation_name",visible:true},
+                {"title" : "Hold","width": "10%",sortable:true,data:"fbl_is_hold",visible:true},
+                {"title" : "Return Date","width": "20%",sortable:true,data:"fdt_sj_return_datetime",visible:true},
+                //{"title" : "S/J Resi No","width": "20%",sortable:true,data:"fst_sj_return_resi_no",visible:true},
+				{"title" : "S/J Resi No","width": "20%",sortable:true,className:'dt-body-center text-center',
+					render: function(data,type,row){
+						return "<a class='btn-resi' href='#'><i class='fa fa-play-circle'></i></a>";
+					}
+				},
+				{"title" : "S/J Return Memo","width": "20%",sortable:true,data:"fst_sj_return_memo",visible:true},
+                {"title" : "S/J Return By ID","width": "20%",sortable:true,data:"fin_sj_return_by_id",visible:true},
+				{"title" : "Unhold Date","width": "20%",sortable:true,data:"fdt_unhold_datetime",visible:true},
+				{"title" : "Unhold","width": "10%",sortable:false,className:'dt-body-center text-center',
 					render: function(data,type,row){
 						return "<a class='btn-unhold' href='#'><i class='fa fa-pause-circle'></i></a>";
 					}
@@ -103,6 +160,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 				onConfirm:function() {
 					
 					doUnhold($(this));
+				}
+			});
+			$(this).confirmation("show");
+		});
+
+		$("#tblMonitoring").on("click",".btn-resi",function(e){
+			e.preventDefault();
+			$(this).confirmation({
+				title:"Update ?",
+				rootSelector: '.btn-resi',
+				onConfirm:function() {
+					doUpdate($(this));
 				}
 			});
 			$(this).confirmation("show");
@@ -139,6 +208,35 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 			}
 		});
 	}
+
+	function upDate($fin_sj_id, $data){
+		t = $('#tblMonitoring').DataTable();
+		var trRow = element.parents('tr');
+		data = t.row(trRow).data();
+		console.log(data);
+
+		$.ajax({
+			url:"<?=site_url() ?>adm_persediaan/monitoring_sj/upDate/" + data.fin_sj_id,
+		}).done(function(resp){
+			if (resp.message != ""){
+				$.alert({
+					title: 'Message',
+					content: resp.message,
+					buttons: {
+						OK : function(){
+							if (resp.status == "SUCCESS"){
+								return;
+							}
+						},
+					}
+				});
+			}
+			if (resp.status == "SUCCESS") {
+				trRow.show();
+			}
+		});
+	}
+
 </script>
 <!-- DataTables -->
 <script src="<?=base_url()?>bower_components/datatables.net/datatables.min.js"></script>
