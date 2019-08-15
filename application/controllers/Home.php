cca68cae50dcee7c7fe3e54951b250f9e1bd03a9
Enny <enny.nuraini06@gmail.com>
2019-08-15 09:59:09 +0700

    enny06 2019/08/15 09.57 Add Dashboard dan Modified GLAccounts (update btn group)

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -2,20 +2,29 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends MY_Controller {
+
+	public function __construct(){
+        parent::__construct();
+        $this->load->library('form_validation');
+        $this->load->model('dashboard_model');
+	}
+	
 	public function index(){
 		$this->load->library("menus");
 		
 		$main_header = $this->parser->parse('inc/main_header',[],true);
 		$main_sidebar = $this->parser->parse('inc/main_sidebar',[],true);
-		//$page_content = $this->parser->parse('pages/sample/template_sample',[],true);
-		$page_content = "";
-		$main_footer = $this->parser->parse('inc/main_footer',[],true);
-		//$control_sidebar = $this->parser->parse('inc/control_sidebar',[],true);
+
+		$this->data["title"] = "Dashboard";
+		$this->data["ttlApproved"] = formatNumber($this->dashboard_model->getTtlApproved());
+		$this->data["ttlNeedApproval"] = formatNumber($this->dashboard_model->getTtlNeedApproval());
+		$this->data["ttlChangeAfterApproved"] = formatNumber($this->dashboard_model->getTtlChangeAfterApproved());
+		$this->data["ttlVoidAuthorize"] = formatNumber($this->dashboard_model->getTtlVoidAuthorize());
+
+		$page_content = $this->parser->parse('pages/dashboard/dashboard', $this->data, true);
+		$main_footer = $this->parser->parse('inc/main_footer', [], true);
+		
 		$control_sidebar = NULL;
-
-		//var_dump($main_sidebar);
-		//return;
-
 		$this->data["MAIN_HEADER"] = $main_header;
 		$this->data["MAIN_SIDEBAR"] = $main_sidebar;
 		$this->data["PAGE_CONTENT"] = $page_content;
---
+++ application/models/dashboard_model.php
@@ -0,0 +1,29 @@
+<?php
+if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class dashboard_model extends CI_Model {
+    //Get Approval List
+    public function getTtlApproved(){
+        $tbl = "";
+        $ssql = "select count(*) as ttl_approve from trverification a
+            inner join users b on a.fin_department_id = b.fin_department_id
+            inner join users c on a.fin_user_group_id = c.fin_group_id
+            where a.fin_rec_id = ? and a.fst_verification_status = 'VF'
+            and b.fst_active = 'A'";
+        $query = $this->db->query($ssql,$this->aauth->get_user_id());
+        $row = $query->row();
+        return $row->ttl_approve;
+    }
+
+    public function getTtlNeedApproval(){
+
+    }
+
+    public function getTtlChangeAfterApproved(){
+
+    }
+
+    public function getTtlVoidAuthorize(){
+
+    }
+}
