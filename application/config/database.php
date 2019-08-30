f2064c7ce1228dd2198001f22ab0679f36b3a50c
Qsystem <devibong@qsystem-online.com>
2019-08-30 18:56:41 +0700

    devibong 20190830 18:56

--- application/config/database.php
+++ application/config/database.php
@@ -138,3 +138,30 @@ $db['default'] = array(
 	'save_queries' => TRUE,
 	'options'=> array(PDO::ATTR_TIMEOUT => 5)
 );
+
+$db['dbsqlsrv'] = array(
+	//'dsn'   => 'sqlsrv:server=127.0.0.1,1433;Database=dbSKG',
+    //'username' => 'sa',
+    //'password' => 'intlsoft',
+	//'dbdriver' => 'pdo',
+	'dsn' => '',
+	'hostname' => '127.0.0.1',
+	'port' => '1433',
+	'username' => 'sa',
+	'password' => 'intlsoft',
+	'database' => 'dbSKG',
+	'dbdriver' => 'sqlsrv',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
