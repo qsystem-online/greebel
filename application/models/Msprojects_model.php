80379d8d2c4ce3091eef986359eed848c4f643d6
Enny <enny.nuraini06@gmail.com>
2019-11-06 12:44:01 +0700

    enny06 2019/11/06 12.43 Add New Master Project

---
+++ application/models/Msprojects_model.php
@@ -0,0 +1,38 @@
+<?php
+if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Msprojects_model extends MY_Model{
+    public $tableName ="msprojects";
+    public $pkey ="fin_project_id";
+
+    public function __construct(){
+        parent::__construct();
+    }
+
+    public function getDataById($fin_project_id){
+        $ssql = "SELECT fin_project_id,fst_project_name FROM msprojects where and fst_active = 'A'";
+        $qr = $this->db->query($ssql, [$fin_project_id]);
+        $rwProjects = $qr->row();
+
+        $data = [
+            "ms_Projects" => $rwProjects
+        ];
+
+        return $data;
+    }
+
+    public function getRules($mode = "ADD", $id = 0){
+        $rules = [];
+
+        $rules[] = [
+            'field' => 'fst_project_name',
+            'label' => 'Project Name',
+            'rules' => 'required',
+            'errors' => array(
+                'required' => '%s tidak boleh kosong'
+            )
+        ];
+
+        return $rules;
+    }
+}
