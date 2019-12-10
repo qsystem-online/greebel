785c80a85c5cbbe1e98a81f414737abc29e05394
Enny <enny.nuraini06@gmail.com>
2019-12-10 16:18:12 +0700

    enny06 2019/12/10 16.17 salah tanda kutip

--- application/views/pages/pr/msrelationgroups/form.php
+++ application/views/pages/pr/msrelationgroups/form.php
@@ -52,7 +52,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 					<input type="hidden" name = "<?=$this->security->get_csrf_token_name()?>" value="<?=$this->security->get_csrf_hash()?>">			
 					<input type="hidden" id="frm-mode" value="<?=$mode?>">
 
-					<div class='form-group'>
+					<div class="form-group">
                     <label for="fin_relation_group_id" class="col-sm-2 control-label"><?=lang("Relation Group ID")?></label>
 						<div class="col-sm-10">
 							<input type="text" class="form-control" id="fin_relation_group_id" placeholder="<?=lang("(Autonumber)")?>" name="fin_relation_group_id" value="<?=$fin_relation_group_id?>" readonly>
