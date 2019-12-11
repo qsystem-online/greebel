64f077d8bf8cea4b0aea389032ce16751687f7b9
Enny <enny.nuraini06@gmail.com>
2019-12-11 10:57:02 +0700

    enny06 2019/12/11 10.57 modified layout kolom

--- application/views/pages/gl/glaccounts/form.php
+++ application/views/pages/gl/glaccounts/form.php
@@ -60,12 +60,11 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
                         <div class="form-group">
                             <label for="fst_glaccount_code" class="col-sm-2 control-label"><?= lang("GL Account Code") ?> * </label>
-                            <div class="col-sm-10">
-                                <input type="text" class="form-control" id="fst_glaccount_code" style="width: unset" placeholder="<?= lang("GL Account Code") ?>" name="fst_glaccount_code" value="<?=$fst_glaccount_code?>">
+                            <div class="col-sm-4">
+                                <input type="text" class="form-control" id="fst_glaccount_code"  placeholder="<?= lang("GL Account Code") ?>" name="fst_glaccount_code" value="<?=$fst_glaccount_code?>">
                                 <div id="fst_glaccount_code_err" class="text-danger"></div>
                             </div>
-                        </div>
-                        <div class="form-group">
+                        
                             <label for="fst_glaccount_level" class="col-sm-2 control-label"><?= lang("Level")?> :</label>
                             <div class="col-sm-4">
                                 <select class="form-control" id="fst_glaccount_level" name="fst_glaccount_level">
@@ -76,9 +75,10 @@ defined('BASEPATH') or exit('No direct script access allowed');
                                 </select>
                             </div>
                         </div>
+
                         <div class="form-group">
                             <label for="fst_glaccount_name" class="col-sm-2 control-label"><?= lang("GL Account Name") ?> :</label>
-                            <div class="col-sm-6">
+                            <div class="col-sm-10">
                                 <input type="text" class="form-control" id="fst_glaccount_name" placeholder="<?= lang("GL Account Name") ?>" name="fst_glaccount_name">
                                 <div id="fst_glaccount_name_err" class="text-danger"></div>
                             </div>
@@ -92,8 +92,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
                                     <option value='C'><?= lang("CREDIT") ?></option>
                                 </select>
                             </div>
-                        </div>
-                        <div class="form-group">
+                        
                             <label for="fin_min_user_level_access" class="col-sm-2 control-label"><?= lang("Min. Level Access")?> :</label>
                             <div class="col-sm-4">
                                 <select class="form-control" id="fin_min_user_level_access" name="fin_min_user_level_access">
@@ -107,6 +106,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
                                 </select>
                             </div>
                         </div>
+
                         <div class="form-group">
                             <label for="select-Currency" class="col-md-2 control-label"><?= lang("Curr Code") ?> :</label>
                             <div class="col-md-4">
@@ -121,15 +121,14 @@ defined('BASEPATH') or exit('No direct script access allowed');
                                 </select>
                                 <!--<div id="fst_curr_code_err" class="text-danger"></div>-->
                             </div>
-                        </div>
-                        <div class="form-group">
 
                             <label for="fin_seq_no" class="col-sm-2 control-label"><?= lang("Seq Number") ?> * </label>
-                            <div class="col-sm-1">
+                            <div class="col-sm-2">
                                 <input type="text" class="form-control" id="fin_seq_no" placeholder="<?= lang("0") ?>" name="fin_seq_no">
                                 <div id="fin_seq_no_err" class="text-danger"></div>
                             </div>
                         </div>
+
                         <div class="form-group">
                             <label for="fbl_is_allow_in_cash_bank_module" class="col-sm-2 control-label"><?= lang("Allow") ?> :</label>
                             <div class="checkbox col-sm-5">
