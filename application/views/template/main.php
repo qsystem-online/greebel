8dbf3164d37658921ca237a9e6e13fe609d301bb
Qsystem <devibong@qsystem-online.com>
2020-08-06 14:02:18 +0700

    logo

--- application/views/template/main.php
+++ application/views/template/main.php
@@ -4,7 +4,7 @@
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<title>AdminLTE 2 | Dashboard</title>
+		<title>Multi | Dashboard</title>
 		<!-- Tell the browser to be responsive to screen width -->
 		<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 		<!-- JQUERY-UI -->
