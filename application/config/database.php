27835fbdf9817d4d71681ed5b3b63553ffcba261
Enny <enny.nuraini06@gmail.com>
2020-10-12 15:41:45 +0700

    @Roni

--- application/config/database.php
+++ application/config/database.php
@@ -69,7 +69,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | The $query_builder variables lets you determine whether or not to load
 | the query builder class.
 */
-$active_group = 'default_local'; 
+$active_group = 'default_qsystem'; 
 $query_builder = TRUE;
 
 $db['multi_qsystem'] = array(
